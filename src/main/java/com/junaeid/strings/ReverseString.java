13cacc87fb945332a87e9e08b4467bd99b41a7b7
junaeid ahmed <junaeidahmed13@gmail.com>
2025-12-17 09:22:45 +0100

    refactored the reverse string class used a reverse string method

--- src/main/java/com/junaeid/strings/ReverseString.java
+++ src/main/java/com/junaeid/strings/ReverseString.java
@@ -2,14 +2,17 @@ package com.junaeid.strings;
 
 public class ReverseString {
     public static void main(String[] args) {
-        StringBuilder reverse_string = new StringBuilder();
         String str = "abcd";
-        System.out.println(str);
+        System.out.println(reverseString(str));
+    }
 
+    public static String reverseString(String str) {
+        StringBuilder reverse_string = new StringBuilder();
         char[] str_to_char = str.toCharArray();
         for (int i = str_to_char.length - 1; i >= 0; i--) {
             reverse_string.append(str_to_char[i]);
         }
-        System.out.println(reverse_string);
+
+        return reverse_string.toString();
     }
 }
