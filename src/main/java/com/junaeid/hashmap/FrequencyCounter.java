d2386160af6ec51c6a09779098f92b8a22ccdab1
junaeid ahmed <junaeidahmed13@gmail.com>
2026-05-22 17:35:32 +0200

    frequency counter using hashmap

---
+++ src/main/java/com/junaeid/hashmap/FrequencyCounter.java
@@ -0,0 +1,19 @@
+package com.junaeid.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrequencyCounter {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 1, 3, 2, 1};
+
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : arr) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        System.out.println(freq);
+    }
+}
